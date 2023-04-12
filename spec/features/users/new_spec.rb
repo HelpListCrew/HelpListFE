30598d4a6373bfbf7acac7743c250bda53cbb0d2
Jade Stewart <jadekstewart3@gmail.com>
2023-04-12 16:07:21 -0600

    feat: Build form for new registrants with radio button option for user type

--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.describe "Register User" do
   describe "As a visitor" do
     before :each do 
-      register_path
+    visit register_path
     end
     describe "when I click on the link to register as a new user" do
       it "takes me to a form to register as a new user" do 
@@ -12,8 +12,8 @@ RSpec.describe "Register User" do
         expect(page).to have_field(:password_confirmation)
 
         expect(page).to have_button("Register")
-        expect(page).to have_checkbox("Donor")
-        expect(page).to have_checkbox("Recipient")
+        expect(page).to have_content("I am registering as a Donor!")
+        expect(page).to have_content("I am registering as a Recipient!")
       end
     end
   end

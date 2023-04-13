2cd65a6ad3b908846a7f6adb82590496bb355f08
Jade Stewart <jadekstewart3@gmail.com>
2023-04-12 20:02:51 -0600

    test: Additional testing for registration of a new user

--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe "Register User" do
-  describe "As a visitor" do
+  describe "As a visitor", :vcr do
     before :each do 
     visit register_path
     end
@@ -14,6 +14,16 @@ RSpec.describe "Register User" do
         expect(page).to have_button("Register")
         expect(page).to have_content("I am registering as a Donor!")
         expect(page).to have_content("I am registering as a Recipient!")
+      end
+
+      it "creates a new user when valid credentials are provided" do
+        fill_in :email, with: "plant_zaddy45@gmail.com"
+        fill_in :password, with: "leafy_greens34"
+        fill_in :password_confirmation, with: "leafy_greens34"
+
+        choose(option: "Donor")
+        click_button "Register"
+        expect(current_path).to eq(dashboard_path)
       end
     end
   end

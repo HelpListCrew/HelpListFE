1f35c319feba5eb55c27dc97b4934a4632d33c4a
Elle Majors <hellolovely@ellemajors.com>
2023-04-15 12:09:00 -0600

    fix: delete orginaztion recipients info from dashbord and add explore section

--- spec/features/users/donors/dashboard_spec.rb
+++ spec/features/users/donors/dashboard_spec.rb
@@ -4,24 +4,15 @@ describe "Donor Dashboard", :vcr do
   describe "As a logged in donor user" do
     describe "when I visit my dashboard" do
       let(:user) {User.new({:data=>{:id=>"5", :type=>"user", :attributes=>{:email=>"octodog86@gmail.com", :user_type=>"donor"}}})}
-      
-      it "displays a recipient's about me section" do
+      it "shows a section to explore organizations near me" do
         allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
         visit dashboard_path
-        save_and_open_page
-        
+
         expect(current_path).to eq(dashboard_path)
-        expect(page).to have_content("About Me")
-      end
-      
-      xit " displays a list of recipients's wishlist items" do
-        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-        visit dashboard_path
-  
-        expect(current_path).to eq(dashboard_path)
-        expect(page).to have_content("#{user.email}'s Wishlist")
+        expect(page).to have_content("Explore Organizations Near Me")
       end
     end
-  end
+  end    
 end      
 

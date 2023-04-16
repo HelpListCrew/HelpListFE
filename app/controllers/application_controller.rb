b355a9a084c7a343cd274f54290e460f0a8f550c
Axel De La Guardia <axeldelaguardia@gmail.com>
2023-04-16 12:47:23 -0600

    fix: find user service returning all users when session[:user_id] was nil

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,16 @@ class ApplicationController < ActionController::Base
 	helper_method :current_user
 
 	def current_user
-		@user ||= HelpListFacade.new.find_user(session[:user_id])
+		if session[:user_id]
+			@user ||= HelpListFacade.new.find_user(session[:user_id])
+		else
+			flash[:error] = "Must be logged in to acces"
+			redirect_to root_path
+		end
+	end
+
+	private
+	def error_message(errors)
+		errors.full_messages.join(', ')
 	end
 end

8fe700da9329670c34edbf150427a51841e29366
Elle Majors <hellolovely@ellemajors.com>
2023-04-20 15:51:35 -0600

    add: feic test

--- spec/features/application_spec/application_html_spec.rb
+++ spec/features/application_spec/application_html_spec.rb
@@ -18,7 +18,6 @@ describe "application.html.erb" do
 		visit root_path
 
 		expect(page).to_not have_link "Dashboard"
-    expect(page).to have_link "Organizations"
 
 		user = User.new({:id=>"5", :type=>"user", :attributes=>{:email=>"octodog86@gmail.com", :user_type=>"donor"}})
 
@@ -26,6 +25,5 @@ describe "application.html.erb" do
 		visit root_path
 
 		expect(page).to have_link("Dashboard")
-		expect(page).to have_link("Organizations")
 	end
 end
